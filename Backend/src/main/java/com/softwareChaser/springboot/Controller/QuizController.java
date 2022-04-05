507d404f34b90c635d2bd533ba2989fc7aabc750
pritamb12 <pritam.b@comakeit.com>
2022-04-05 09:30:12 +0530

    changes

--- Backend/src/main/java/com/softwareChaser/springboot/Controller/QuizController.java
+++ Backend/src/main/java/com/softwareChaser/springboot/Controller/QuizController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,7 +53,8 @@ public class QuizController {
 	{
 		return new ResponseEntity<>(Qservice.fetchByDifficulty(Difficulty),HttpStatus.OK);
 	}	
-//@crossOrigin(origins="http://localhost:3000/"
+	
+    @CrossOrigin(origins="http://localhost:3000/")
 	@GetMapping("/quiz/CategoryAndDifficulty") 
 	public ResponseEntity<List<Question>> fetchByCategoryAndDifficulty(@RequestParam String Category,@RequestParam String Difficulty) 
 	{ 
--- Backend/src/main/java/com/softwareChaser/springboot/Model/Question.java
+++ Backend/src/main/java/com/softwareChaser/springboot/Model/Question.java
@@ -11,6 +11,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 
 
